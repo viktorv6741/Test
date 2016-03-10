f496f6c58181bea37562cda16edb79efee1f93ab
Vitya <viktorv6741@gmail.com>
2016-03-10 02:21:08 +0200

    lalala

---
+++ Test.java
@@ -0,0 +1,123 @@
+1. Which specification that provides runtime environment in which java byte code can be executed?
+
+B) JVM  //java byte code can only be executed in Java Virtul Machine.
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+2. Java language was initially called as "OAK" ?
+
+A) true
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+3. Which provides a new way for your programs to interface with code libraries written in other languages?
+
+B) JNI
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+4. The comment must begin with /* and end with */ is known as ?
+
+C) Both A & B  // can be also single line  /* ... coments ... */ 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+5. Which is nothing but a blueprint or a template for creatng different objects which defines its properties and behaviours?
+
+B) A class 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+6. An exception is an event, which occurs during the execution of the program that dirupt the normal flow of the program's instructions?
+
+A) true
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+7. What output you will get if you run this program?
+
+class Modulus{
+	int x = 42;
+	double y = 42.25;
+	System.out.println("x mod 10 = " + x%10); // We will see 2   42 / 10 = 4.2
+	System.out.println("y mod 10 = " + y%10); // We will see 225  42.25 / 10 = 4.225
+	
+	//% cals modulus, and returns remainder of the division 
+ }
+ 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+8. Who is also called father of Java Programming Language?
+
+// Difficult to answer. But probably, Dennis Richie, because he one of those who created C laguage. Java inherited syntactic from C. 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+9. Which statement provides an easy way to dispatch execution to differental parts of your code based on the value of an expression? 
+
+B) switch // I think this is correct answer
+//for excemple : 
+int a, b;
+char symbol; 
+switch (symbol){
+	case '*':
+	sout(a * b);
+	break;
+	case '-':
+	sout(a - b);
+	break;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+10. Give the output for the following program?
+
+class Char{
+	public static void main(Strng[] args){
+		
+		char ch1, ch2;
+		ch1 = 88;
+		ch2 = 'Y';
+		sout("ch1 + ch2: ")    // will be  -> ch1 + ch2: 
+		sout(ch1 + " " + ch2); // Actualy, I do not remember what means 88 in ASCII table... so  ->  'some symbol'  Y;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+11. Static fields belong to the class, not instances of the class?
+
+A)true //  We don't need to create objects to get static members of the class
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+12. The following syntax is used to declare,
+[access_modifier][static][final] type_name = [initial_value]; ?
+
+//looks something like: 
+public static final int = 8; 
+
+// The following syntax is used to declare some fields
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
